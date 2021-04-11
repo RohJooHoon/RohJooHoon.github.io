0fd2c9216612f541e3d0c95a2398848316417057
노주훈 <joohoon.roh@gmail.com>
2021-04-12 00:47:42 +0900

    수정사항 반영

--- project/euro/js/common.js
+++ project/euro/js/common.js
@@ -85,10 +85,14 @@ function parallax(degree) {
             const startPoint = $target.offset().top - $(window).height();
             const endPoint = $target.offset().top + $target.outerHeight();
             if (startPoint < windowScrollTop && windowScrollTop < endPoint) {
-                const nowPoint = windowScrollTop - startPoint;
-                const gab = endPoint - startPoint;
-                const percentage = (nowPoint / gab).toFixed(4);
-                $target.css('background-position-y', percentage * 100 + "%");
+                if (degree == "auto") {
+                    $target.css('background-position-y', windowScrollTop * 0.5 + "px");
+                } else {
+                    const nowPoint = windowScrollTop - startPoint;
+                    const gab = endPoint - startPoint;
+                    const percentage = (nowPoint / gab).toFixed(4);
+                    $target.css('background-position-y', percentage * 100 + "%");
+                }
             }
         });
     }
