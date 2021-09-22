d75ce999d906ddd180d7e3ba8d5f80252856a602
노주훈 <joohoon.roh@gmail.com>
2021-09-22 23:43:13 +0900

    체감기온 + 불쾌지수 보정값

--- project/study_react/src/index.js
+++ project/study_react/src/index.js
@@ -4,6 +4,8 @@ import './css/index.css';
 import App from './App';
 import App_c from './App_c';
 
+mongodb+srv://joohoon:<password>@cluster0.abygv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority0p
+
 ReactDOM.render(
     <React.StrictMode>
         <App/>
