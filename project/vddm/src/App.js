913ac6406e106019bc6a57e4323c32e48e8a5105
노주훈 <joohoon.roh@gmail.com>
2021-12-22 18:38:23 +0900

    react 기본 정리

---
+++ project/vddm/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './css/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        react
+      </header>
+    </div>
+  );
+}
+
+export default App;
