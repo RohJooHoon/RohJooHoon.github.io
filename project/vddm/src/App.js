2a9033caafb1e329184189455968dcc708ddddba
노주훈 <joohoon.roh@gmail.com>
2021-12-25 00:01:22 +0900

    기본 동작 적용

--- project/vddm/src/App.js
+++ project/vddm/src/App.js
@@ -1,8 +1,34 @@
+import React, { useState } from "react";
 import './css/layout.css';
 import './css/common.css';
 
 function App() {
-    let title = 'dddd';
+    let [list, listSet] = useState([
+        {
+            name: "강남 고기 맛집",
+            date: "2월 17일 발행",
+            like: 0
+        },
+        {
+            name: "신촌 닭발 맛집",
+            date: "2월 18일 발행",
+            like: 0
+        },
+        {
+            name: "종로 곱창 맛집",
+            date: "2월 19일 발행",
+            like: 0
+        },
+    ]);
+    function listFunc(type, index) {
+        let newList = [...list];
+        if (type == "changeTitle") {
+            newList[index].name = '제목을 입력하세요';
+        } else if (type == "likeUp") {
+            newList[index].like = newList[index].like  + 1;
+        }
+        listSet(newList);
+    }
     return (
         <>
             <header className='header'>
@@ -10,7 +36,7 @@ function App() {
                     <div className='headerLeft'>
                         <a className='headerLogo' href='#'></a>
                     </div>
-                    <div className='headerTitle'>{title}</div>
+                    <div className='headerTitle'>{list[0].name}</div>
                     <div className='headerRight'>
 
                     </div>
@@ -18,7 +44,23 @@ function App() {
             </header>
             <div className="body">
                 <div className="bodyInner">
-                    sdfsdf
+                    <ul className="list">
+                        <li className="listItem" key="0">
+                            <button onClick={()=>{listFunc("changeTitle", 0)}}>제목 변경</button>
+                            <div className="listTitle">{list[0].name}<button onClick={() => {listFunc("likeUp", 0)}}> 👍</button><span>{list[0].like}</span></div>
+                            <div className="listDate">{list[0].date}</div>
+                        </li>
+                        <li className="listItem" key="1">
+                            <button onClick={()=>{listFunc("changeTitle", 1)}}>제목 변경</button>
+                            <div className="listTitle">{list[1].name}<button onClick={() => {listFunc("likeUp", 1)}}> 👍</button><span>{list[1].like}</span></div>
+                            <div className="listDate">{list[1].date}</div>
+                        </li>
+                        <li className="listItem" key="2">
+                            <button onClick={()=>{listFunc("changeTitle", 2)}}>제목 변경</button>
+                            <div className="listTitle">{list[2].name}<button onClick={() => {listFunc("likeUp", 2)}}> 👍</button><span>{list[2].like}</span></div>
+                            <div className="listDate">{list[2].date}</div>
+                        </li>
+                    </ul>
                 </div>
             </div>
             <div className="footer">
