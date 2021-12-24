e3eb15f1a127f4c1db0d485f0c0b827c697a9f91
노주훈 <joohoon.roh@gmail.com>
2021-12-24 23:30:10 +0900

    기본 구조 정리

--- project/vddm/src/App.js
+++ project/vddm/src/App.js
@@ -1,14 +1,31 @@
-import logo from './logo.svg';
-import './css/App.css';
+import './css/layout.css';
+import './css/common.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        react
-      </header>
-    </div>
-  );
+    let title = 'dddd';
+    return (
+        <>
+            <header className='header'>
+                <div className='headerInner'>
+                    <div className='headerLeft'>
+                        <a className='headerLogo' href='#'></a>
+                    </div>
+                    <div className='headerTitle'>{title}</div>
+                    <div className='headerRight'>
+
+                    </div>
+                </div>
+            </header>
+            <div className="body">
+                <div className="bodyInner">
+                    sdfsdf
+                </div>
+            </div>
+            <div className="footer">
+                sdfdsf
+            </div>
+        </>
+    );
 }
 
 export default App;
