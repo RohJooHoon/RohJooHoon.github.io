8fc4b911748020915d52d3f197ac1d6d655a1792
노주훈 <joohoon.roh@gmail.com>
2021-12-28 18:26:12 +0900

    api로 리스트 draw

--- project/vddm/src/component/page/Main/Main.js
+++ project/vddm/src/component/page/Main/Main.js
@@ -1,8 +1,27 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import axios from "axios";
 import 'css/main.css';
+import MainListItem from "component/page/Main/MainListItem";
 
 function Main(props) {
+    let listGap = 10; // 요청개수
+    let [list, listSet] = useState([]);
+
+    // 리스트 데이터 조회
+    function getList() {
+        axios.get(`https://devapi.kiwisnap.net/vddm/product/list?start=${list.length}&length=${listGap}`,{
+            headers: {
+                Authorization: 'Bearer ' + localStorage.getItem('token'),
+            },
+        }).then(result => {
+            listSet([...list, ...result.data]);
+        });
+    }
+
+    useEffect(() => {
+        console.log("list : ", list);
+    }, [list]);
+
     useEffect(() => {
         // 레이아웃 세팅
         props.layoutSet({
@@ -17,25 +36,17 @@ function Main(props) {
             },
         });
 
-        // 리스트 데이터 조회
-        axios.get(`https://devapi.kiwisnap.net/vddm/list?start=0&length=10`,{
-            headers: {
-                Authorization: 'Bearer ' + localStorage.getItem('token'),
-            },
-        }).then(result => {
-            console.log("list result : ", result);
-        });
+        if (!list.length) {
+            getList();
+        }
     }, []);
     return (
         <div className="mainInner">
             <ul className="mainThumbnailList">
-                <li className="mainThumbnailItem"></li>
-                <li className="mainThumbnailItem"></li>
-                <li className="mainThumbnailItem"></li>
-                <li className="mainThumbnailItem"></li>
-                <li className="mainThumbnailItem"></li>
-                <li className="mainThumbnailItem"></li>
-                <li className="mainThumbnailItem"></li>
+                {listDraw}
+                {list.map(function (value, index) {
+                    return <MainListItem value={value} key={list.length + index} onClick={getList}></MainListItem>
+                })}
             </ul>
         </div>
     );
--- project/vddm/src/component/page/MyMenu/MyMenu.js
+++ project/vddm/src/component/page/MyMenu/MyMenu.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import 'css/main.css';
 
 function MyMenu(props) {
