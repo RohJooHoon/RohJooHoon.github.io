85fad42f61340940cc5bc48fdc613982a89a5dfc
노주훈 <joohoon.roh@gmail.com>
2021-12-29 11:54:08 +0900

    list draw 방식 변경 (스크롤 끌려올라감 방지)

--- project/vddm/src/component/page/Main/Main.js
+++ project/vddm/src/component/page/Main/Main.js
@@ -1,27 +1,8 @@
-import React, {useEffect, useState} from "react";
-import axios from "axios";
+import React, {useEffect} from "react";
 import 'css/main.css';
-import MainListItem from "component/page/Main/MainListItem";
+import MainList from "component/page/Main/MainList";
 
 function Main(props) {
-    let listGap = 10; // 요청개수
-    let [list, listSet] = useState([]);
-
-    // 리스트 데이터 조회
-    function getList() {
-        axios.get(`https://devapi.kiwisnap.net/vddm/product/list?start=${list.length}&length=${listGap}`,{
-            headers: {
-                Authorization: 'Bearer ' + localStorage.getItem('token'),
-            },
-        }).then(result => {
-            listSet([...list, ...result.data]);
-        });
-    }
-
-    useEffect(() => {
-        console.log("list : ", list);
-    }, [list]);
-
     useEffect(() => {
         // 레이아웃 세팅
         props.layoutSet({
@@ -35,18 +16,10 @@ function Main(props) {
                 onType: 'main'
             },
         });
-
-        if (!list.length) {
-            getList();
-        }
     }, []);
     return (
         <div className="mainInner">
-            <ul className="mainThumbnailList">
-                {list.map(function (value, index) {
-                    return <MainListItem value={value} key={list.length + index} onClick={getList}></MainListItem>
-                })}
-            </ul>
+            <MainList></MainList>
         </div>
     );
 }
