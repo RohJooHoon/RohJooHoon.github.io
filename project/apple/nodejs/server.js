2e3cc94a710fcf38ff6a065585ba664c6aab2e15
노주훈 <joohoon.roh@gmail.com>
2022-01-31 23:51:56 +0900

    node.js 기본 세팅

--- project/apple/nodejs/server.js
+++ project/apple/nodejs/server.js
@@ -1,6 +1,29 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
-app.listen(8080, function (){
+app.use(bodyParser.urlencoded({express : true}));
 
+app.listen(8080, function (){
+    console.log('listening on 8080');
 });
+
+// 라우터
+app.get('/', function (req, res){
+    res.sendFile(__dirname + '/index.html');
+});
+app.get('/write', function (req, res){
+    res.sendFile(__dirname + '/write.html');
+});
+
+// form 데이터
+app.post('/add', function (req, res) {
+    // res.send('전송완료');
+    console.log("req.body : ", req.body);
+})
+
+// 페이지에 텍스트 삽입
+app.get('/dada', function (req, res){
+    res.send("나는 다다 입니다.");
+})
+
