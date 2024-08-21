c0bf44f5d80c42e7fb5ae9b797e9f6650d8020df
노주훈 <joohoon.roh@gmail.com>
2024-08-21 11:21:18 +0900

    네비게이션 수정

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,24 +1,38 @@
 "use client";
 
-import { useEffect } from "react";
 import { useCommonStore } from "@/store/Common";
 import classNames from "classnames";
 import styles from "./index.module.css";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import Image from "next/legacy/image";
+
+// import material icons
+import DehazeIcon from "@mui/icons-material/Dehaze";
 
 export default function () {
   const path = usePathname();
   const { isOpenMenu, isOpenMenuEnd, toggleMenu } = useCommonStore();
 
+  const handleNavWrapClick = (event: React.MouseEvent<HTMLDivElement>) => {
+    if (!(event.target instanceof HTMLAnchorElement)) {
+      toggleMenu();
+    }
+  };
+
   return (
     <div className={classNames(styles.header, { [styles.is_open]: isOpenMenu, [styles.is_openEnd]: isOpenMenuEnd })}>
       <div className={styles.headerWrap}>
+        <Link className={styles.headerLogo} href="/">
+          <Image src="/images/logo.png" alt="Logo" width={22} height={22} />
+          PORTFOLIO
+        </Link>
+
         <button className={styles.headerMenuButton} key="menu-button" onClick={toggleMenu}>
-          menu
+          <DehazeIcon style={{ fontSize: 24, color: "var(--white)" }} />
         </button>
 
-        <div className={styles.headerNavWrap}>
+        <div className={styles.headerNavWrap} onClick={handleNavWrapClick}>
           <nav className={styles.headerNav}>
             <Link className={classNames(styles.headerNavItem, { [styles.is_active]: path === "/" })} href="/">
               Home
