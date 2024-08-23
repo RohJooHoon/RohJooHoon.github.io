ca89489aa9b21dbefc1c4c8f64549ea142e44fba
노주훈 <joohoon.roh@gmail.com>
2024-08-23 09:37:41 +0900

    터치 이벤트 추가

--- src/app/page.component.tsx
+++ src/app/page.component.tsx
@@ -5,9 +5,9 @@ import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 
 export function Banner({ onShowMoreClick }: { onShowMoreClick: () => void }) {
   const [windowWidth, setWindowWidth] = useState(0);
-  const sliderRef = useRef<HTMLUListElement>(null);
+  const sliderRef = useRef<HTMLDivElement>(null);
   const [startX, setStartX] = useState(0);
-  const [nowPage, setNowPage] = useState(1);
+  const [nowPage, setNowPage] = useState(2);
   const [moveDiff, setMoveDiff] = useState(-nowPage * 100);
   const [isDragging, setIsDragging] = useState(false);
   const [isResetting, setIsResetting] = useState(false);
@@ -18,23 +18,22 @@ export function Banner({ onShowMoreClick }: { onShowMoreClick: () => void }) {
     }
   }, []);
 
-  const handleMouseDown = (e: React.MouseEvent) => {
-    e.preventDefault();
-    setStartX(e.clientX);
+  const handleStart = (clientX: number) => {
+    setStartX(clientX);
     setIsDragging(true);
   };
 
-  const handleMouseMove = (e: MouseEvent) => {
+  const handleMove = (clientX: number) => {
     if (isDragging) {
-      const diffX = e.clientX - startX;
+      const diffX = clientX - startX;
       setMoveDiff(-nowPage * 100 + (diffX / windowWidth) * 100);
     }
   };
 
-  const handleMouseUp = (e: MouseEvent) => {
+  const handleEnd = (clientX: number) => {
     setIsDragging(false);
     if (sliderRef.current) {
-      const diffX = e.clientX - startX;
+      const diffX = clientX - startX;
       let getNowPage = nowPage;
 
       if (Math.abs((diffX / windowWidth) * 100) > 10) {
@@ -50,26 +49,55 @@ export function Banner({ onShowMoreClick }: { onShowMoreClick: () => void }) {
         setMoveDiff(-getNowPage * 100);
       }
 
-      if (getNowPage < 1) {
+      if (getNowPage < 2) {
         setTimeout(() => {
-          const lastPage = bannerItems.length - 2;
+          const lastPage = bannerItems.length - 3 + (getNowPage - 1);
           setIsResetting(true);
           setNowPage(lastPage);
           setMoveDiff(-lastPage * 100);
           setTimeout(() => {
             setIsResetting(false);
-          }, 50);
+          }, 10);
         }, 300);
-      } else if (getNowPage > bannerItems.length - 2) {
+      } else if (getNowPage > bannerItems.length - 3) {
         setTimeout(() => {
+          const startPage = 2 + (getNowPage - 1) - (bannerItems.length - 3);
           setIsResetting(true);
-          setNowPage(1);
-          setMoveDiff(-1 * 100);
+          setNowPage(2);
+          setMoveDiff(-2 * 100);
           setTimeout(() => {
             setIsResetting(false);
-          }, 50);
+          }, 10);
         }, 300);
       }
+    }
+  };
+
+  const handleMouseDown = (e: React.MouseEvent) => {
+    e.preventDefault();
+    handleStart(e.clientX);
+  };
+
+  const handleMouseMove = (e: MouseEvent) => {
+    handleMove(e.clientX);
+  };
+
+  const handleMouseUp = (e: MouseEvent) => {
+    handleEnd(e.clientX);
+  };
+
+  const handleTouchStart = (e: React.TouchEvent) => {
+    e.preventDefault();
+    handleStart(e.touches[0].clientX);
+  };
+
+  const handleTouchMove = (e: TouchEvent) => {
+    handleMove(e.touches[0].clientX);
+  };
+
+  const handleTouchEnd = (e: TouchEvent) => {
+    if (e.changedTouches && e.changedTouches.length > 0) {
+      handleEnd(e.changedTouches[0].clientX);
     }
   };
 
@@ -77,13 +105,19 @@ export function Banner({ onShowMoreClick }: { onShowMoreClick: () => void }) {
     if (isDragging) {
       window.addEventListener("mousemove", handleMouseMove);
       window.addEventListener("mouseup", handleMouseUp);
+      window.addEventListener("touchmove", handleTouchMove);
+      window.addEventListener("touchend", handleTouchEnd);
     } else {
       window.removeEventListener("mousemove", handleMouseMove);
       window.removeEventListener("mouseup", handleMouseUp);
+      window.removeEventListener("touchmove", handleTouchMove);
+      window.removeEventListener("touchend", handleTouchEnd);
     }
     return () => {
       window.removeEventListener("mousemove", handleMouseMove);
       window.removeEventListener("mouseup", handleMouseUp);
+      window.removeEventListener("touchmove", handleTouchMove);
+      window.removeEventListener("touchend", handleTouchEnd);
     };
   }, [isDragging]);
 
@@ -94,11 +128,11 @@ export function Banner({ onShowMoreClick }: { onShowMoreClick: () => void }) {
   ];
 
   // 앞 뒤에 더미 생성
-  bannerItems.unshift(bannerItems[bannerItems.length - 1]);
-  bannerItems.push(bannerItems[1]);
+  bannerItems.unshift(bannerItems[bannerItems.length - 2], bannerItems[bannerItems.length - 1]);
+  bannerItems.push(bannerItems[2], bannerItems[3]);
 
   return (
-    <div className={styles.banner}>
+    <div className={styles.banner} ref={sliderRef} onMouseDown={handleMouseDown} onTouchStart={handleTouchStart}>
       <div className={styles.bannerText}>
         <strong className={styles.bannerTextTitle}>
           Hello
@@ -107,9 +141,16 @@ export function Banner({ onShowMoreClick }: { onShowMoreClick: () => void }) {
         </strong>
       </div>
 
-      <ul className={styles.bannerSlider} ref={sliderRef} onMouseDown={handleMouseDown}>
+      <ul className={styles.bannerSlider}>
         {bannerItems.map((item, index) => (
-          <li key={index} className={styles.bannerSliderItem} style={{ transform: `translateX(${moveDiff}%)`, transition: isDragging || isResetting ? "none" : "transform 0.3s" }}>
+          <li
+            key={index}
+            className={styles.bannerSliderItem}
+            style={{
+              transform: `translateX(${moveDiff}%)`,
+              transition: isDragging || isResetting ? "none" : "transform 0.3s",
+            }}
+          >
             <Image src={item.src} alt={item.alt} layout="fill" objectFit="cover" />
           </li>
         ))}
